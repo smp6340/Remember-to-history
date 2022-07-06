186a2c60a28e2d3ac98c6990bbf56a3630072116
smp6340 <smp6340@naver.com>
2022-07-06 11:37:05 +0900

    ✨: 함수 재구성

--- src/functions/Quiz/index.js
+++ src/functions/Quiz/index.js
@@ -3,57 +3,47 @@ import React, { useState } from "react";
 //Data
 import { data } from "../../data/data";
 
-//Style
-import * as S from "./styles";
+const [currentQuestion, setCurrentQuestion] = useState(0);
+const [myAnswer, setMyAnswer] = useState("");
+const [score, setScore] = useState(0);
+const [finish, setFinish] = useState(false);
+const [show, setShow] = useState(false);
+const [clickAnswer, setClickAnswer] = useState(false);
 
+export function answerCheck() {
+    setMyAnswer(variant);
+    setClickAnswer(true);
+};
 
-const Quiz = () => {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [myAnswer, setMyAnswer] = useState("");
-    const [score, setScore] = useState(0);
-    const [finish, setFinish] = useState(false);
-    const [show, setShow] = useState(false);
-    const [clickAnswer, setClickAnswer] = useState(false);
+//문제를 맞췄을 경우에 점수를 +1시켜 저장하는 함수
+export function correctAnswerCheck(){
+    if(myAnswer === data[currentQuestion].answer){
+        setScore(score + 1);
+    }
+};
 
-    //사용자가 어떤 정답을 선택했는지 보여주는 함수
-    const answerCheck = () => {
-        setMyAnswer(variant);
-        setClickAnswer(true);
-    };
+//정답공개 함수
+export function showAnswer() {
+    setShow(true);
+};
 
-    //문제를 맞췄을 경우에 점수를 +1시켜 저장하는 함수
-    const correctAnswerCheck = () => {
-        if(myAnswer === data[currentQuestion].answer){
-            setScore(score + 1);
+//사용자가 선택한 정답을 초기화하는 함수
+export function reset() {
+    setShow(false);
+    setClickAnswer(false);
+};
+
+//문제 풀이가 끝났는지 결정하는 함수
+export function finishHandler() {
+        if(currentQuestion === data.length - 1){
+            setFinish(true);
         }
-    };
+};
 
-    //정답공개 함수
-    const showAnswer = () => {
-        setShow(true);
-    };
-
-    //사용자가 선택한 정답을 초기화하는 함수
-    const reset = () => {
-        setShow(false);
-
-        setClickAnswer(false);
-    };
-
-    //문제 풀이가 끝났는지 결정하는 함수
-    const finishHandler = () => {
-            if(currentQuestion === data.length - 1){
-                setFinish(true);
-            }
-    };
-
-    //게임을 재시작하는 함수
-    const startOver = () => {
-        setCurrentQuestion(0);
-        setFinish(false);
-        setMyAnswer("");
-        setScore(0);
-    };
-}
-
-export default Quiz;
+//게임을 재시작하는 함수
+export function startOver() {
+    setCurrentQuestion(0);
+    setFinish(false);
+    setMyAnswer("");
+    setScore(0);
+};
--- src/pages/Finish/index.js
+++ src/pages/Finish/index.js
@@ -4,11 +4,12 @@ import React from "react";
 import * as S from "./styles";
 
 //Functions
-import Quiz from "../../functions/Quiz";
+import * as Quiz from "../../functions/Quiz/index";
 
 const Finish = () => {
     return(
         <>
+        
         </>
     );
 }
