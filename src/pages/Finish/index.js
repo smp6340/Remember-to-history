4994acb4e2194f4bb364100376b3ecd9677b0684
smp6340 <smp6340@naver.com>
2022-07-07 18:40:14 +0900

    ✨: Finish 페이지 문구 추가

--- src/pages/Finish/index.js
+++ src/pages/Finish/index.js
@@ -6,11 +6,17 @@ import * as S from "./styles";
 //Functions
 import * as Quiz from "../../functions/Quiz/index";
 
+//Data
+import { data } from "../../data/data";
+
 const Finish = () => {
     return(
-        <>
-        
-        </>
+        <div>
+            <div>
+                <div>{'문제 풀이가 끝났습니다. 최종 점수는 ${score}/${data.length - 1}입니다.'}</div>
+                <button onClick={() => Quiz.startOver()}>처음으로</button>
+            </div>
+        </div>
     );
 }
 
