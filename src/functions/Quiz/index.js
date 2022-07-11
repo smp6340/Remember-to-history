9961a2be2fa2ede571d578e6c672506155a5256d
smp6340 <smp6340@naver.com>
2022-07-11 15:46:56 +0900

    ✨ : 조건 형식 페이지 구성

--- src/functions/Quiz/index.js
+++ src/functions/Quiz/index.js
@@ -3,12 +3,21 @@ import React, { useState } from "react";
 //Data
 import { data } from "../../data/data";
 
+//Page
+import Finish from "../../pages/Finish";
+
 const [currentQuestion, setCurrentQuestion] = useState(0);
 const [myAnswer, setMyAnswer] = useState("");
 const [score, setScore] = useState(0);
 const [finish, setFinish] = useState(false);
 const [show, setShow] = useState(false);
 const [clickAnswer, setClickAnswer] = useState(false);
+
+if(finish){
+    return(
+        <Finish />
+    );
+}
 
 //사용자가 선택한 정답을 보여주는 함수
 export function answerCheck() {
--- src/pages/Problem/index.js
+++ src/pages/Problem/index.js
@@ -3,10 +3,16 @@ import React from "react";
 //Style
 import * as S from "./styles";
 
+//Function
+import Quiz from "../../functions/Quiz";
+
 const problem = () => {
     return(
-        <>
-        </>
+        <div>
+            <div>
+                <h2></h2>
+            </div>
+        </div>
     );
 }
 
